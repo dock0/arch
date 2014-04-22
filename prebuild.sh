eec8b230baefe71f49d45fd59fd6948115b5614e
Les Aker <me@lesaker.org>
2014-04-22 07:37:46 -0400

    fix privileged flag

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd $(dirname "${BASH_SOURCE[0]}")
-docker run -privileged -t -i -v $(pwd):/opt/mkimage dock0/arch /opt/mkimage/mkimage.sh
+docker run --privileged -t -i -v $(pwd):/opt/mkimage dock0/arch /opt/mkimage/mkimage.sh
 
