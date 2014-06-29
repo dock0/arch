2473b175d4636e49007a220f20a97c320e759326
Les Aker <me@lesaker.org>
2014-06-29 17:43:08 -0400

    clean up prebuild script for copypasta

--- prebuild.sh
+++ prebuild.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 cd $(dirname "${BASH_SOURCE[0]}")
-docker run --privileged -t -i \
-    -v $SSH_AUTH_SOCK:/auth.sock -e SSH_AUTH_SOCK=/auth.sock \
-    -v $(pwd):/opt/mkimage \
-    dock0/arch \
-    /opt/mkimage/mkimage.sh
+# Start the container
+#   With privilege
+#   Interactively in the foreground
+#   With my SSH agent
+#   With this repo as /opt/mkimage
+#   Run the mkimage.sh script
+docker run --privileged -t -i -v $SSH_AUTH_SOCK:/auth.sock -e SSH_AUTH_SOCK=/auth.sock -v $(pwd):/opt/mkimage dock0/arch /opt/mkimage/mkimage.sh
 
